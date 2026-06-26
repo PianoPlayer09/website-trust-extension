b2b3f6127dbd7e43f4b4c821f5a7444266384e47
DVC <ansh09feb@gmail.com>
2026-06-26 13:50:29 -0400

    some changes and gitignore

--- popup.js
+++ popup.js
@@ -1,6 +1,32 @@
-document.getElementById("checkBtn")
-.addEventListener("click", () => {
+async function getCurrentTab() {
 
-    alert("Analyzing website...");
+  const [tab] = await chrome.tabs.query({
 
-});
+    active: true,
+
+    currentWindow: true
+
+  });
+
+  return tab;
+
+}
+
+function getDomain(url){
+    try{
+    const parsedUrl = new URL(url);
+    return parsedUrl.hostname;
+        }catch (error){
+            return "Invalid URL"
+        }
+    }
+
+
+async function main() {
+    const tab = await getCurrentTab();
+    const domain = getDomain(tab.url);
+    document.getElementById("domain").textContent = domain;
+    // document.getElementById("url").textContent = tab.url;
+}
+
+main()
